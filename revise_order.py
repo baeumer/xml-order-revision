7e0be248d6f5cb8dfb7e3419fc522212e0ac6142
baeumer <34710768+baeumer@users.noreply.github.com>
2017-12-20 13:46:26 +0100

    Add files via upload

---
+++ revise_order.py
@@ -0,0 +1,37 @@
+import xml.etree.ElementTree as ET
+import time
+tree = ET.parse('Versandliste.xml')
+root = tree.getroot()
+
+Termine = root[1]
+
+Beispieldatum = root[1][0][11].text
+print "Beispieldatum:", Beispieldatum
+
+Suchtitel = raw_input("Suchtitel   : ")
+Start_NEU = raw_input("Start - NEU : ")
+Ende_NEU = raw_input("Ende - NEU  : ")
+
+Fehler = 0
+
+i = 0
+for Termin in Termine:
+    Titel = Termin.find('titel').text
+    if len(Start_NEU) != len (Termin.find('terminvon').text):
+        print "Neuer Text mit falscher Laenge"
+        Fehler = 1
+        break
+    if len(Ende_NEU) != len (Termin.find('terminbis').text):
+        print "Neuer Text mit falscher Laenge"
+        Fehler = 1
+        break
+    if Titel == Suchtitel:
+        i = i + 1
+        Termin.find('terminvon').text = Start_NEU
+        Termin.find('terminbis').text = Ende_NEU
+
+if Fehler == 0:
+    print i, "Aenderungen vorgenommen"
+    tree.write(open('Versandliste2.xml', 'w'))
+
+time.sleep(3)
